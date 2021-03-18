fa6e98dc0262d9269cba07ca75b07317495a31c0
Vinicius Catarino <vinicius_catarino@outlook.com>
2021-03-18 14:31:56 -0300

    added fonts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Image } from "react-native";
 
-import { Container } from "./styles";
+import LogoIgm from "../../assets/logo.png";
+
+import { Container, Title } from "./styles";
 
 const SignIn: React.FC = () => {
   return (
     <>
-      <Container />
+      <Container>
+        <Image source={LogoIgm} />
+
+        <Title>Faça sei logon</Title>
+      </Container>
     </>
   );
 };
