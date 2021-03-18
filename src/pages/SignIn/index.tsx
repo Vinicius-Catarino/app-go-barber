e1d5704a683aee8b2b51006a717f44c26baf473b
Vinicius Catarino <vinicius_catarino@outlook.com>
2021-03-18 15:08:19 -0300

    Added input and button components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Image } from "react-native";
 
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
 import LogoIgm from "../../assets/logo.png";
 
 import { Container, Title } from "./styles";
@@ -12,6 +15,17 @@ const SignIn: React.FC = () => {
         <Image source={LogoIgm} />
 
         <Title>Faça sei logon</Title>
+
+        <Input name="email" icon="mail" placeholder="Email" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+
+        <Button
+          onPress={() => {
+            console.log("FOI");
+          }}
+        >
+          Entrar
+        </Button>
       </Container>
     </>
   );
